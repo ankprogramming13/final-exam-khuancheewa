b508b3980fa03ad81631c4453429fdcd0ced4323
Unknown <tremajor415@gmail.com>
2018-02-22 12:30:28 +0700

    exam1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "stdio.h"
+#include "string.h"
+using namespace std;
+int main(void)
+{
+    int();
+    printf("welcome to CLI Calculator V1.00");
+{
 
-int main() {
 
-        return 0;
+        int a,b,cmd;
+        cout << "menu" << endl;
+        cout << "1.+" << endl;
+        cout << "2.-" << endl;
+        cout << "3.*" << endl;
+        cout << "4./" << endl;
+        cout << "5.quit" << endl;
+
+
+
+}
+
+
+      return 0;
 }
